03223f8ef797360b284ca9b80f82713856af7626
Mark Joemarie Obra <dev.obra2005@gmail.com>
2025-04-26 18:16:49 +0800

    update

---
+++ resume.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="resume.css">
+    <title>Resume</title>
+</head>
+<body>
+    <main>
+    <nav>
+            <div class="logo">Obra.</div>
+             <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="gallery.html">Gallery</a></li>
+                <li><a href="resume.php">Resume</a></li>
+             </ul>
+        </nav>
+
+
+    </main>
+</body>
+</html>
